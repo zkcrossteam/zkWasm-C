a3bdb80b00c4854de263b6ae438e77375b69d0d3
sinka <xgao@zoyoe.com>
2023-02-21 17:04:33 +1100

    update rlp algorithm

--- sdk/c/rlp/include/rlp.h
+++ sdk/c/rlp/include/rlp.h
@@ -1,4 +1,5 @@
 #ifndef __ZKWASM_RLP__
+
 #define __ZKWASM_RLP__
 
 #define SINGLE_CHAR 1
@@ -8,15 +9,25 @@
 #define LONG_LIST 5
 #define INVALID 0
 
+#define EVENT_ARRAY_SIZE 12
+#define TOPIC_COUNT_IDX 0
+#define DATA_EXIST_IDX 1
+#define DATA_EXIST 1
+#define TOPIC_START_IDX 2
+#define TOPIC_LEN_IDX 3
+#define DATA_START_IDX 10
+#define DATA_LEN_IDX 11
+#define EVENT_LIST_DEPTH 3
+
+#define SIG_LEN 32
+#define ADDR_LEN 20
+
 #include <stdbool.h>
 #include <zkwasmsdk.h>
 
-uint8_t ruleType(uint8_t e);
-
 struct rlpItem {
-    uint8_t startPos;
+    uint32_t startPos;
     uint8_t isString;
-    uint16_t padding;
     // theoretically rlp can support a single stirng up to 2^64
     int len;
     struct rlpItem *firstChild;
@@ -24,18 +35,18 @@ struct rlpItem {
 };
 
 struct rlpItemAllocator {
-    struct rlpItem items[128];
-    uint8_t pos;
+    struct rlpItem items[1024];
+    uint32_t pos;
 };
 
 
 struct rlpItem* allocRlpItem(struct rlpItemAllocator *itemAllocator);
 
-static inline int decodeLength(unsigned char *stream, int start, int length) {
+static inline int decodeLength(uint8_t *stream, int start, int end) {
     int sum = 0;
-    for(int i = 0; i < length; i++) {
+    for(; start <= end; start++) {
         sum *= 256;
-        sum += stream[start + i];
+        sum += stream[start];
     }
     return sum;
 }
@@ -53,7 +64,7 @@ static inline uint8_t decodeType(uint8_t *stream, int start, int *lengthBytes, i
     }
     else if(ch >= 0xb8 && ch <= 0xbf) {
         *lengthBytes = stream[start] - 0xb7;
-        *dataLength = decodeLength(stream, start + 1, *lengthBytes);
+        *dataLength = decodeLength(stream, start + 1, start + *lengthBytes);
         return LONG_STRING;
     }
     else if(ch >= 0xc0 && ch <= 0xf7) {
@@ -63,11 +74,11 @@ static inline uint8_t decodeType(uint8_t *stream, int start, int *lengthBytes, i
     }
     else if(ch >= 0xf8 && ch <= 0xff) {
         *lengthBytes = stream[start] - 0xf7;
-        *dataLength = decodeLength(stream, start + 1, *lengthBytes);
+        *dataLength = decodeLength(stream, start + 1, start + *lengthBytes);
         return LONG_LIST;
     }
     else return INVALID;
 }
 
-struct rlpItem *decode(unsigned char *stream, int start, struct rlpItemAllocator *itemAllocator);
+struct rlpItem *decode(uint8_t *stream, int start, struct rlpItemAllocator *itemAllocator);
 #endif
--- sdk/c/rlp/lib/rlp.c
+++ sdk/c/rlp/lib/rlp.c
@@ -1,70 +1,75 @@
 #include "rlp.h"
 
-struct rlpItem *decodeString(uint8_t *stream, int start, int length, struct rlpItemAllocator *itemAllocator) {
+struct rlpItem* allocRlpItem(struct rlpItemAllocator *itemAllocator) {
+    return &itemAllocator->items[itemAllocator->pos];
+}
+
+
+struct rlpItem *decodeString(uint8_t *stream, int start, int end, struct rlpItemAllocator *itemAllocator) {
     struct rlpItem *curr = allocRlpItem(itemAllocator);
     curr->isString = true;
     curr->firstChild = NULL;
     curr->next = NULL;
-    curr->len = length;
-    curr->startPos = start;
+    if(start > end) {
+        // empty string
+        curr->len = 0;
+        curr->startPos = -1;
+    } else {
+        curr->len = end - start + 1;
+        curr->startPos = start;
+    }
+    itemAllocator->pos++;
     return curr;
 }
 
-struct rlpItem* allocRlpItem(struct rlpItemAllocator *itemAllocator) {
-    uint8_t pos = itemAllocator->pos;
-    itemAllocator->pos++;
-    return &itemAllocator->items[pos-1];
-}
+struct rlpItem *decodeList(uint8_t *stream, int start, int end, struct rlpItemAllocator *itemAllocator, int *listLength) {
+    // empty list
+    if(start > end) return NULL;
 
-struct rlpItem *decodeList(uint8_t *stream, int start, int length, struct rlpItemAllocator *itemAllocator, int *listLength) {
-    if(length == 0) {
-        return NULL;
-    } else {
-        struct rlpItem *rlpFirstChild = allocRlpItem(itemAllocator);
-        int index = start;
-        int end = index + length;
-        int listSize = 0;
-        struct rlpItem *cursor = rlpFirstChild;
-        while(index < end) {
-            int lengthBytes, dataLength;
-            decodeType(stream, index, &lengthBytes, &dataLength);
-            struct rlpItem *curr = decode(stream, index, itemAllocator);
-            if(index != start) {
-                cursor->next = curr;
-                cursor = cursor->next;
-            }
-            index += 1 + lengthBytes + dataLength;
-            listSize++;
+    struct rlpItem *rlpFirstChild = allocRlpItem(itemAllocator);
+    int index = start;
+    int listSize = 0;
+    struct rlpItem *cursor = rlpFirstChild;
+    while(index <= end) {
+        int lengthBytes, dataLength;
+        decodeType(stream, index, &lengthBytes, &dataLength);
+        struct rlpItem *curr = decode(stream, index, itemAllocator);
+        if(index != start) {
+            cursor->next = curr;
+            cursor = cursor->next;
         }
-        *listLength = listSize;
-        //check same level tranverse correction
-        assert(index == end);
-        return rlpFirstChild;
+        index += 1 + lengthBytes + dataLength;
+        listSize++;
     }
+    *listLength = listSize;
+    //check same level tranverse correction
+    assert(index == end + 1);
+    return rlpFirstChild;
 }
 
-struct rlpItem *decode(unsigned char *stream, int start, struct rlpItemAllocator *itemAllocator) {
+struct rlpItem *decode(uint8_t *stream, int start, struct rlpItemAllocator *itemAllocator) {
     int lengthBytes, dataLength;
     int8_t type = decodeType(stream, start, &lengthBytes, &dataLength);
 
-    //printf("decode[%u] start %u, lbytes %u, length %u\n", type, start, lengthBytes, dataLength);
     if(type == SINGLE_CHAR) {
-        return decodeString(stream, start, 0, itemAllocator);
+        return decodeString(stream, start, start, itemAllocator);
     } else if(type == SHORT_STRING) {
-        return decodeString(stream, start + 1, dataLength, itemAllocator);
+        return decodeString(stream, start + 1, start + dataLength, itemAllocator);
     } else if(type == LONG_STRING) {
-        return decodeString(stream, start + 1 + lengthBytes, dataLength, itemAllocator);
+        return decodeString(stream, start + 1 + lengthBytes, start + lengthBytes + dataLength, itemAllocator);
     } else if(type == SHORT_LIST) {
         struct rlpItem *curr = allocRlpItem(itemAllocator);
         curr->isString = false;
-        curr->firstChild = decodeList(stream, start + 1, dataLength, itemAllocator, &curr->len);
+        itemAllocator->pos++;
+        curr->firstChild = decodeList(stream, start + 1, start + dataLength, itemAllocator, &curr->len);
         curr->startPos = -1;
         curr->next = NULL;
         return curr;
     } else if(type == LONG_LIST) {
         struct rlpItem *curr = allocRlpItem(itemAllocator);
         curr->isString = false;
-        curr->firstChild = decodeList(stream, start + lengthBytes + 1, dataLength, itemAllocator, &curr->len);
+        itemAllocator->pos++;
+        curr->firstChild = decodeList(stream, start + lengthBytes + 1, start + lengthBytes + dataLength, itemAllocator, &curr->len);
         curr->startPos = -1;
         curr->next = NULL;
         return curr;
