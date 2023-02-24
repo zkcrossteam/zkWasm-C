398331a4428fc94f05686a6742f01165b6e6a8f6
sinka <xgao@zoyoe.com>
2023-02-25 02:53:39 +1100

    using global buf for communication between ts and c

--- hybrid/inner.ts
+++ hybrid/inner.ts
@@ -1,3 +1,13 @@
-export function inc(a: i32): i32{
-    return a + 1;
+export function crash(
+  message: string | null,
+  fileName: string | null,
+  lineNumber: u32,
+  columnNumber: u32,
+): void {
+  unreachable();
+}
+
+declare function get_byte(x: u32): u32;
+export function compare(): i32{
+    return get_byte(0) - get_byte(1);
 }
--- hybrid/main.c
+++ hybrid/main.c
@@ -4,11 +4,20 @@
 #include <stddef.h>
 #include "rlp.h"
 
-extern uint32_t inc(uint32_t a);
+extern uint32_t compare();
+uint32_t c[2];
+
+__attribute__((visibility("default")))
+uint32_t get_byte(uint32_t i) {
+    return c[i];
+}
 
 __attribute__((visibility("default")))
 int zkmain() {
     uint32_t a = wasm_input(1);
-    require(inc(a) > 10);
+    uint32_t b = wasm_input(1);
+    c[0] = a;
+    c[1] = b;
+    require(compare() == 0);
     return 0;
 }
