a95a45f0d51f942904c3cec864c792ec5f2cce93
Sinka <xgao@zoyoe.com>
2023-03-20 20:25:40 +0800

    bugfix for bls.c

--- sdk/c/bls/lib/bls.c
+++ sdk/c/bls/lib/bls.c
@@ -5,7 +5,7 @@ void blspair(uint64_t* g1, uint64_t* g2, uint64_t* g3) {
         blspair_g1(g1[i]);
     }
     for(i=0;i<33;i++) {
-        blspair_g2(g1[i]);
+        blspair_g2(g2[i]);
     }
     for(i=0;i<48;i++) {
         g3[i] = blspair_pop();
@@ -13,10 +13,10 @@ void blspair(uint64_t* g1, uint64_t* g2, uint64_t* g3) {
 }
 
 void blssum(uint32_t size, uint64_t* g1, uint64_t *gr) {
-    int i;
+    int i, j;
     for (i=0; i<size; i++) {
-        for(i=0;i<17;i++) {
-            blssum_g1(g1[i]);
+        for(j=0; j<17; j++) {
+            blssum_g1(g1[i*17+j]);
         }
     }
     for(i=0;i<17;i++) {
