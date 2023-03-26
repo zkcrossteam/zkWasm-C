1c0cbdf89d3a5a8d52c389520b3dca1abf5cb794
Sinka <xgao@zoyoe.com>
2023-03-26 18:34:13 +0800

    fix sdk/ecc

--- sdk/c/ecc/include/bn254.h
+++ sdk/c/ecc/include/bn254.h
@@ -9,10 +9,10 @@ void bn254pair_g1(uint64_t x);
 void bn254pair_g2(uint64_t x);
 uint64_t bn254pair_pop(void);
 
-void bn254sum_g1(uint64_t x);
-uint64_t bn254sum_pop(void);
+void bn254msm_g1(uint64_t x);
+uint64_t bn254msm_pop(void);
 
 void bn254pair(uint64_t* g1, uint64_t* g2, uint64_t* g3);
-void bn254sum(uint32_t size, uint64_t* g1, uint64_t *gr);
+void bn254msm(uint32_t size, uint64_t* g1, uint64_t *gr);
 
 #endif
