f7a7e93fede6ec9f58672ce68faeab5042c1c685
Sinka <xgao@zoyoe.com>
2023-04-27 21:39:06 +1000

    add test.sh for kvpair

---
+++ tests/kvpair/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+rm -rf output
+mkdir output
+
+# Single test
+RUST_LOG=info ~/zkWasm/target/release/cli  -k 20 --function zkmain --output ./output --wasm output.wasm setup
+
+RUST_LOG=info ~/zkWasm/target/release/cli  -k 20 --function zkmain --output ./output --wasm output.wasm single-prove
+RUST_LOG=info ~/zkWasm/target/release/cli  -k 20 --function zkmain --output ./output --wasm output.wasm single-verify --proof output/zkwasm.0.transcript.data
