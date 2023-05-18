287cea7f3b02c5685b2b0cd117108bd03a042fe3
sinka <xgao@zoyoe.com>
2023-05-18 18:17:30 +1000

    patch c implementation

--- sdk/c/kvpair/include/kvpair.h
+++ sdk/c/kvpair/include/kvpair.h
@@ -1,29 +1,39 @@
 #include <stdint.h>
 
-void kvpair_addr(uint64_t x);
+void kvpair_setroot(uint64_t x);
+void kvpair_address(uint64_t x);
 void kvpair_set(uint64_t x);
 uint64_t kvpair_get(void);
+uint64_t kvpair_getroot(void);
 
 #define DATA64_SIZE 4
-#define DATA8_SIZE (DATA64_SIZE * 4)
+#define DATA8_SIZE (DATA64_SIZE * 8)
 
-
-
-static __inline__ void kv_get(uint64_t *treeid, const uint32_t key, uint64_t *data)
+static __inline__ void kv_setroot(uint64_t *root)
 {
-  kvpair_addr(treeid[0]);
-  kvpair_addr(treeid[1]);
-  kvpair_addr(key);
+  for (int i=0; i<DATA64_SIZE; i++) {
+    kvpair_setroot(root[i]);
+  }
+}
+
+static __inline__ void kv_getroot(uint64_t *root, uint64_t *data)
+{
+  for (int i=0; i<DATA64_SIZE; i++) {
+    data[i] = kvpair_getroot();
+  }
+}
+
+static __inline__ void kv_get(const uint32_t key, uint64_t *data)
+{
+  kvpair_address(key);
   for (int i=0; i<DATA64_SIZE; i++) {
     data[i] = kvpair_get();
   }
 }
 
-static __inline__ void kv_set(uint64_t *treeid, const uint32_t key, uint64_t *data)
+static __inline__ void kv_set(const uint32_t key, uint64_t *data)
 {
-  kvpair_addr(treeid[0]);
-  kvpair_addr(treeid[1]);
-  kvpair_addr(key);
+  kvpair_address(key);
   for (int i=0; i<DATA64_SIZE; i++) {
     kvpair_set(data[i]);
   }
--- tests/kvpair/test.c
+++ tests/kvpair/test.c
@@ -5,12 +5,13 @@
 
 __attribute__((visibility("default")))
 int zkmain() {
-    uint64_t address[3];
-    address[2] = 1;
+    uint64_t root[4];
+    uint64_t address = 1;
     uint64_t buf[8];
     buf[0] = 1;
-    kv_set(address, address[2], buf); 
-    kv_get(address, address[2], buf); 
+    kv_setroot(root);
+    kv_set(address, buf);
+    kv_get(address, buf);
     require(buf[0] == 1);
     return 0;
 }
