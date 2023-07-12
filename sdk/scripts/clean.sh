d6e18d51cb34dc6c0c161669d21f1130d58c889a
Antony <badc0de57721@gmail.com>
2023-07-12 18:31:58 +0800

    adjust clean.sh

--- sdk/scripts/clean.sh
+++ sdk/scripts/clean.sh
@@ -7,8 +7,24 @@ TOP_PATH=$SCRIPT_PATH/..
 
 MAKEFILE=$SCRIPT_PATH/Makefile
 
-make clean -C $TOP_PATH/c/sdk/lib -f $MAKEFILE
-make clean -C $TOP_PATH/c/rlp/lib -f $MAKEFILE
-make clean -C $TOP_PATH/c/hash/lib -f $MAKEFILE
-make clean -C $TOP_PATH/c/ecc/lib -f $MAKEFILE
-#make clean -C $TOP_PATH/c/kvpair/lib -f $MAKEFILE
+DIR_SDK_LIB="$TOP_PATH/c/sdk/lib"
+DIR_RLP_LIB="$TOP_PATH/c/rlp/lib"
+DIR_HASH_LIB="$TOP_PATH/c/hash/lib"
+DIR_ECC_LIB="$TOP_PATH/c/ecc/lib"
+DIR_KVPAIR_LIB="$TOP_PATH/c/kvpair/lib"
+
+if [ -d "$DIR_SDK_LIB" ]; then
+    make clean -C $TOP_PATH/c/sdk/lib -f $MAKEFILE
+fi
+if [ -d "$DIR_RLP_LIB" ]; then
+    make clean -C $TOP_PATH/c/rlp/lib -f $MAKEFILE
+fi
+if [ -d "$DIR_HASH_LIB" ]; then
+    make clean -C $TOP_PATH/c/hash/lib -f $MAKEFILE
+fi
+if [ -d "$DIR_ECC_LIB" ]; then
+    make clean -C $TOP_PATH/c/ecc/lib -f $MAKEFILE
+fi
+if [ -d "$DIR_KVPAIR_LIB" ]; then
+    make clean -C $TOP_PATH/c/kvpair/lib -f $MAKEFILE
+fi
