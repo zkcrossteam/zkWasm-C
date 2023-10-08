75b1f96a55ebf17f884978c9c1442a05bcb44f62
sinka <xgao@zoyoe.com>
2023-10-08 21:02:43 +1100

    update bn254

--- sdk/c/ecc/include/bn254.h
+++ sdk/c/ecc/include/bn254.h
@@ -9,8 +9,10 @@ void bn254pair_g1(uint64_t x);
 void bn254pair_g2(uint64_t x);
 uint64_t bn254pair_pop(void);
 
-void bn254msm_g1(uint64_t x);
-uint64_t bn254msm_pop(void);
+void bn254_sum_new(uint64_t x);
+void bn254_sum_g1(uint64_t x);
+void bn254_sum_coeff(uint64_t x);
+uint64_t bn254_sum_finalize(void);
 
 void bn254pair(uint64_t* g1, uint64_t* g2, uint64_t* g3);
 void bn254msm(uint32_t size, uint64_t* g1, uint64_t *gr);
--- sdk/c/ecc/lib/bn254.c
+++ sdk/c/ecc/lib/bn254.c
@@ -21,11 +21,15 @@ void bn254msm(uint32_t size, uint64_t* g1, uint64_t *gr) {
     int i, j;
     for (i=0; i<size; i++) {
         // prepare point and scalar
-        for(j=0; j<(LIMBSZ * 2 + 1 + 4); j++) {
-            bn254msm_g1(g1[MSMLIMB*i+j]);
+        bn254_sum_new(i==0);
+        for(j=0; j<4; j++) {
+            bn254_sum_coeff(g1[MSMLIMB*i+j]);
         }
-    }
-    for(i=0; i<(LIMBSZ * 2 + 1); i++) {
-        gr[i] = bn254msm_pop();
+        for(j=4; j<(LIMBSZ * 2 + 1); j++) {
+            bn254_msm_g1(g1[MSMLIMB*i+j]);
+        }
+        for(i=0; i<(LIMBSZ * 2 + 1); i++) {
+            gr[i] = bn254_sum_finalize();
+        }
     }
 }
